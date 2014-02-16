c5cee457db10883e1ed75ca4ff1fdc906d2b31c5
tofiksa <tofiksa@gmail.com>
2014-02-16 21:42:38 +0100

    fix

--- Hvaskjer/src/no/hioa/hvaskjer/MainScreenActivity.java
+++ Hvaskjer/src/no/hioa/hvaskjer/MainScreenActivity.java
@@ -6,16 +6,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 
-/*import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MotionEvent;*/
 
 public class MainScreenActivity extends Activity{
 
         
         protected boolean _active = true;
-        //protected int _splashTime = 3000; // time to display the splash screen in ms
         
      // Splash screen timer
         private static int SPLASH_TIME_OUT = 3000;
@@ -45,37 +40,6 @@ public class MainScreenActivity extends Activity{
             }, SPLASH_TIME_OUT);
         }
         
-        
-        /*
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.splash);
-                
-                Thread splashTread = new Thread() {
-                    @Override
-                    public void run() {
-                        try {
-                            int waited = 0;
-                            while(_active && (waited < _splashTime)) {
-                                sleep(100);
-                                if(_active) {
-                                    waited += 100;
-                                }
-                            }
-                        } catch(InterruptedException e) {
-                            // do nothing
-                        } finally {                        	
-                            finish();
-                                              
-                            Intent i = new Intent("no.hioa.hvaskjer.AllProductsActivity");
-                            startActivity(i);
-                            minStop();
-                        }
-                    }
-                };
-                splashTread.start();
-        }*/
         
         public void minStop() {
             _active = false;
